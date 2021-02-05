f0cf837ab908cca15417d8cca0952a0938038fdf
Ivan <iizdepskyy112@gmail.com>
2021-02-05 18:23:04 +0200

    12_saving_user_to_database_bckend

--- hoaxifyBackEnd/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ hoaxifyBackEnd/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -2,8 +2,13 @@ package com.hoaxify.hoaxify;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.hoaxify.hoaxify.user.UserRepository;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.internal.MethodSorter;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -18,20 +23,42 @@ import com.hoaxify.hoaxify.user.User;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserControllerTest {
+
+    private static final String API_1_0_USERS = "/api/1.0/users";
 
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Before
+    public void cleanup() {
+        userRepository.deleteAll();
+    }
+
     @Test
     public void postUser_whenUserIsValid_receiveOk() {
+        User user = createValidUser();
+        ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    private User createValidUser() {
         User user = new User();
         user.setUsername("test-user");
         user.setDisplayName("test-display");
-        user.setPassword("Password");
+        user.setPassword("P4ssword");
+        return user;
+    }
 
-        ResponseEntity<Object> response = testRestTemplate.postForEntity("/api/1.0/users", user, Object.class);
+    @Test
+    public void postUser_whenUserIsValid_userSavedToDatabase() {
+        User user = createValidUser();
+        testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+        assertThat(userRepository.count()).isEqualTo(1);
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
