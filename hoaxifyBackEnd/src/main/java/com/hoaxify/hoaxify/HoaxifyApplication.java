e3be24819756d62e4bced831eaa9137658063609
Ivan <iizdepskyy112@gmail.com>
2021-02-05 17:43:28 +0200

    11_post_user_backend

--- hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
+++ hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
@@ -2,11 +2,20 @@ package com.hoaxify.hoaxify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+
 public class HoaxifyApplication {
 
+
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(HoaxifyApplication.class, args);
+
 	}
+
+
+
 }
