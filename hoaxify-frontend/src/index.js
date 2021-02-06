50888cb4daca491be17c7a8a75d596036b501c42
Ivan <iizdepskyy112@gmail.com>
2021-02-06 09:45:20 +0200

    15_user_signupPage_frontend

--- hoaxify-frontend/src/index.js
+++ hoaxify-frontend/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import { UserSignupPage } from './pages/UserSignupPage';
 
 ReactDOM.render(
   <React.StrictMode>
