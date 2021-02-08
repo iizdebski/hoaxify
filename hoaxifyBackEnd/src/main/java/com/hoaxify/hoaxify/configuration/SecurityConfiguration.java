5db2d83892ad6293ea36140f6f9bff1cc98f62da
Ivan <iizdepskyy112@gmail.com>
2021-02-08 20:09:47 +0200

    32_login_errors_backend

--- hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/configuration/SecurityConfiguration.java
+++ hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/configuration/SecurityConfiguration.java
@@ -6,17 +6,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
-        http.httpBasic();
+        http.httpBasic().authenticationEntryPoint(new BasicAuthenticationEntryPoint());
 
-        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/1.0/login").authenticated()
+        http
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/api/1.0/login").authenticated()
                 .and()
                 .authorizeRequests().anyRequest().permitAll();
     }
+
 }
