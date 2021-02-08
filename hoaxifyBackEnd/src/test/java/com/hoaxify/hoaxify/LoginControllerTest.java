3a3d8fc7d0852ff5b3c56c2826b6d2cdebe5443a
Ivan <iizdepskyy112@gmail.com>
2021-02-08 21:52:15 +0200

    34

--- hoaxifyBackEnd/src/test/java/com/hoaxify/hoaxify/LoginControllerTest.java
+++ hoaxifyBackEnd/src/test/java/com/hoaxify/hoaxify/LoginControllerTest.java
@@ -2,12 +2,16 @@ package com.hoaxify.hoaxify;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.hoaxify.hoaxify.user.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
@@ -15,6 +19,11 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.hoaxify.hoaxify.error.ApiError;
+import com.hoaxify.hoaxify.user.UserRepository;
+import com.hoaxify.hoaxify.user.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -25,6 +34,18 @@ public class LoginControllerTest {
 
     @Autowired
     TestRestTemplate testRestTemplate;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
+
+    @Before
+    public void cleanup() {
+        userRepository.deleteAll();
+        testRestTemplate.getRestTemplate().getInterceptors().clear();
+    }
 
     @Test
     public void postLogin_withoutUserCredentials_receiveUnauthorized() {
@@ -58,6 +79,26 @@ public class LoginControllerTest {
         assertThat(response.getHeaders().containsKey("WWW-Authenticate")).isFalse();
     }
 
+    @Test
+    public void postLogin_withValidCredentials_receiveOk() {
+        userService.save(TestUtil.createValidUser());
+        authenticate();
+        ResponseEntity<Object> response = login(Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void postLogin_withValidCredentials_receiveLoggedInUserId() {
+        User inDB = userService.save(TestUtil.createValidUser());
+        authenticate();
+        ResponseEntity<Map<String, Object>> response = login(new ParameterizedTypeReference<Map<String, Object>>() {});
+
+        Map<String, Object> body = response.getBody();
+        Integer id = (Integer) body.get("id");
+        assertThat(id).isEqualTo(inDB.getId());
+    }
+
+
     private void authenticate() {
         testRestTemplate.getRestTemplate()
                 .getInterceptors().add(new BasicAuthenticationInterceptor("test-user", "P4ssword"));
@@ -65,6 +106,10 @@ public class LoginControllerTest {
 
     public <T> ResponseEntity<T> login(Class<T> responseType){
         return testRestTemplate.postForEntity(API_1_0_LOGIN, null, responseType);
+    }
+
+    public <T> ResponseEntity<T> login(ParameterizedTypeReference<T> responseType){
+        return testRestTemplate.exchange(API_1_0_LOGIN, HttpMethod.POST ,null, responseType);
     }
 
 }
