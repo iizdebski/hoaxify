ad25ddb9b80b552842e4a4a446133306351a89a7
Ivan <iizdepskyy112@gmail.com>
2021-02-09 20:45:35 +0200

    44_login_success_routing_frontend

--- hoaxify-frontend/src/index.js
+++ hoaxify-frontend/src/index.js
@@ -4,12 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
 import App from './containers/App';
-import * as apiCalls from './api/apiCalls';
-
-
-const actions = {
-    postLogin: apiCalls.login
-}
 
 ReactDOM.render(
     <HashRouter>
