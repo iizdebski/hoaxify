adbdd65b39ceb86f596a95057489a1b4fd1a11d6
Ivan <iizdepskyy112@gmail.com>
2021-02-09 11:08:07 +0200

    35_user_view_model_backend

--- hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/user/LoginController.java
+++ hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/user/LoginController.java
@@ -1,5 +1,6 @@
 package com.hoaxify.hoaxify.user;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.hoaxify.hoaxify.shared.CurrentUser;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,13 +8,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class LoginController {
 
     @PostMapping("/api/1.0/login")
-    Map<String, Object> handleLogin(@CurrentUser User loggedInUser) {
-        return Collections.singletonMap("id", loggedInUser.getId());
+    @JsonView(Views.Base.class)
+    User handleLogin(@CurrentUser User loggedInUser) {
+        return loggedInUser;
     }
 }
--- hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/user/User.java
+++ hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/user/User.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -27,21 +28,27 @@ public class User implements UserDetails{
 
     @Id
     @GeneratedValue
+    @JsonView(Views.Base.class)
     private long id;
 
     @NotNull(message = "{hoaxify.constraints.username.NotNull.message}")
     @Size(min = 4, max=255)
     @UniqueUsername
+    @JsonView(Views.Base.class)
     private String username;
 
     @NotNull
     @Size(min = 4, max=255)
+    @JsonView(Views.Base.class)
     private String displayName;
 
     @NotNull
     @Size(min = 8, max=255)
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message="{hoaxify.constraints.password.Pattern.message}")
     private String password;
+
+    @JsonView(Views.Base.class)
+    private String image;
 
     @Override
     @Transient
