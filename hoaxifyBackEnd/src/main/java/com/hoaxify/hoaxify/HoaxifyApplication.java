73aa5a5cc0e37cf4214459e55379cfe5ebbdc562
Ivan <iizdepskyy112@gmail.com>
2021-02-11 19:24:47 +0200

    65_creating_random_users_backend

--- hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
+++ hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
@@ -1,7 +1,15 @@
 package com.hoaxify.hoaxify;
 
+import com.hoaxify.hoaxify.user.User;
+import com.hoaxify.hoaxify.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import javax.print.attribute.IntegerSyntax;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class HoaxifyApplication {
@@ -10,4 +18,19 @@ public class HoaxifyApplication {
 		SpringApplication.run(HoaxifyApplication.class, args);
 	}
 
+	@Bean
+	@Profile("!test")
+	CommandLineRunner run(UserService userService) {
+		return (args) -> {
+				IntStream.rangeClosed(1, 15)
+						.mapToObj(i -> {
+							User user = new User();
+							user.setUsername("user"+i);
+							user.setDisplayName("display"+i);
+							user.setPassword("P4ssword");
+							return user;
+						})
+						.forEach(userService::save);
+		};
+	}
 }
