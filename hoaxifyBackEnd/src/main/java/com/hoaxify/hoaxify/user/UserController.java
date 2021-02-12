a827aaba2e01816a66a110b526119cde29daaa13
Ivan <iizdepskyy112@gmail.com>
2021-02-12 17:47:40 +0200

    74_user_get_api_frontend

--- hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/user/UserController.java
+++ hoaxifyBackEnd/src/main/java/com/hoaxify/hoaxify/user/UserController.java
@@ -40,6 +40,13 @@ public class UserController {
         return userService.getUsers(loggedInUser, page).map(UserVM::new);
     }
 
+    @GetMapping("/users/{username}")
+    UserVM getUserByName(@PathVariable String username ) {
+        User user = userService.getByUsername(username);
+        return new UserVM(user);
+    }
+
+
     @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     ApiError handleValidationException(MethodArgumentNotValidException exception, HttpServletRequest request) {
